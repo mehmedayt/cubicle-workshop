a9cace4e0980dba8e1e75af03bd9fc55e0f43cba
mehmedayt <Mehmed_ayt@abv.bg>
2024-01-19 01:19:34 +0200

    Add accessory page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -2,24 +2,6 @@ const uniqid = require('uniqid');
 const Cube = require('../models/Cube');
 
 
-// const cubes = [
-//     {
-//         id: '1n73sh8holhz66elc',
-//         name: 'Mirror Cube',
-//         description: 'A cool mirror cube',
-//         imageUrl: 'https://m.media-amazon.com/images/I/71TrvUl50OL.jpg',
-//         difficultyLevel: 4
-//     },
-//     {
-//         id: '2n73sh8holaz66elc',
-//         name: 'Rubic Classic',
-//         description: 'Evergreen',
-//         imageUrl: 'https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067-675x380.jpg',
-//         difficultyLevel: 3
-//     }
-// ];
-
-
 exports.getAll = async (search, from, to) => {
     let result = await Cube.find().lean();
 
