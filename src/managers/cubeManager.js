de070675647e1e67511dcc09022e7c542b4996dd
mehmedayt <Mehmed_ayt@abv.bg>
2024-01-18 17:27:52 +0200

    Get cubes from DB

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,25 +1,27 @@
 const uniqid = require('uniqid');
-
-const cubes = [
-    {
-        id: '1n73sh8holhz66elc',
-        name: 'Mirror Cube',
-        description: 'A cool mirror cube',
-        imageUrl: 'https://m.media-amazon.com/images/I/71TrvUl50OL.jpg',
-        difficultyLevel: 4
-    },
-    {
-        id: '2n73sh8holaz66elc',
-        name: 'Rubic Classic',
-        description: 'Evergreen',
-        imageUrl: 'https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067-675x380.jpg',
-        difficultyLevel: 3
-    }
-];
+const Cube = require('../models/Cube');
 
 
-exports.getAll = (search, from, to) => {
-    let result = cubes.slice();
+// const cubes = [
+//     {
+//         id: '1n73sh8holhz66elc',
+//         name: 'Mirror Cube',
+//         description: 'A cool mirror cube',
+//         imageUrl: 'https://m.media-amazon.com/images/I/71TrvUl50OL.jpg',
+//         difficultyLevel: 4
+//     },
+//     {
+//         id: '2n73sh8holaz66elc',
+//         name: 'Rubic Classic',
+//         description: 'Evergreen',
+//         imageUrl: 'https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067-675x380.jpg',
+//         difficultyLevel: 3
+//     }
+// ];
+
+
+exports.getAll = async (search, from, to) => {
+    let result = await Cube.find().lean();
 
     if(search){
         result = result.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()))
@@ -36,15 +38,12 @@ exports.getAll = (search, from, to) => {
     return result;
 }
 
-exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
+exports.getOne = (cubeId) => Cube.findById(cubeId);
 
-exports.create = (cubeData) => {
-    const newCube = {
-        id: uniqid(),
-        ...cubeData,
-    };
+exports.create = async (cubeData) => {
+    const cube = new Cube(cubeData);
 
-    cubes.push(newCube);
+    await cube.save();
 
-    return newCube;
+    return cube;
 };
