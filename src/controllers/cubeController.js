f42741a690269b9c80120732ede867406f9240f7
mehmedayt <Mehmed_ayt@abv.bg>
2024-01-26 02:28:47 +0200

    Add delete cube page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -5,7 +5,7 @@ const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => { 
     console.log(req.user) ;
-    res.render('create');
+    res.render('cube/create');
 });
 
 router.post('/create', async (req, res) => {
@@ -34,7 +34,7 @@ router.get('/:cubeId/details', async (req, res) => {
     if (!cube) {
         return res.redirect('404');
     }
-    res.render('details', { cube });
+    res.render('cube/details', { cube });
 });
 
 
@@ -54,6 +54,12 @@ router.post('/:cubeId/attach-accessory', async (req, res) => {
     await cubeManager.attachAccessory(cubeId, accessoryId);
 
     res.redirect(`/cubes/${cubeId}/details`);
+});
+
+router.get('/:cubeId/delete', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+
+    res.render('cube/delete', { cube });
 });
 
 
