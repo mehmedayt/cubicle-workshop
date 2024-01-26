4fe0064b27e5c76537a0934a1e4d6438d553017c
mehmedayt <Mehmed_ayt@abv.bg>
2024-01-26 03:55:43 +0200

    Add update cube

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -49,6 +49,7 @@ router.get('/:cubeId/attach-accessory', async (req, res) => {
 
 router.post('/:cubeId/attach-accessory', async (req, res) => {
     const { accessory: accessoryId } = req.body;
+    
     const cubeId = req.params.cubeId;
 
     await cubeManager.attachAccessory(cubeId, accessoryId);
@@ -62,5 +63,24 @@ router.get('/:cubeId/delete', async (req, res) => {
     res.render('cube/delete', { cube });
 });
 
+router.post('/:cubeId/delete', async (req, res) => {
+    await cubeManager.delete(req.params.cubeId);
+
+    res.redirect('/');
+});
+
+router.get('/:cubeId/edit', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+
+    res.render('cube/edit', { cube });
+});
+
+router.post('/:cubeId/edit', async (req, res) => {
+    const cubeData = req.body;
+
+    await cubeManager.update(req.params.cubeId, cubeData);
+
+    res.redirect(`/cubes/${req.params.cubeId}/details`);
+})
 
 module.exports = router;
